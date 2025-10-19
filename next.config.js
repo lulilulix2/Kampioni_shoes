a68d50198da30d1646da234ee9c0a47f6d3be3f0
lulilulix2 <lulzimhoxhaks@yahoo.com>
2025-10-19 14:14:22 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    turbopack: false, // Disable Turbopack for Amplify compatibility
+  },
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
